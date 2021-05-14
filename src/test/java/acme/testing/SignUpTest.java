040ac3a24be554761551373b247de24836a7cd5b
javrodsan5 <javrodsan5@alum.us.es>
2021-05-14 12:05:18 +0200

    Comienzo de tests de dashboard

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -12,9 +12,41 @@
 
 package acme.testing;
 
-public class SignUpTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.By;
+
+public class SignUpTest extends AcmeTest{
 
 	// This is a placeholder where you can introduce your own sign-up test
   	// Note that it depends on your project-specific test class.
+	
+	
+	@Override
+	@BeforeAll
+	public void beforeAll() {
+		super.beforeAll();
+
+		super.setBaseCamp("http", "localhost", "8080", "/Starter-Project", "/master/welcome", "?language=en&debug=true");
+		super.setAutoPausing(true);
+
+		this.navigateHome();
+		this.signIn("administrator", "administrator");
+		super.clickOnMenu("Administrator", "Populate DB (initial)");
+		this.signOut();
+	}
+
+	public void signIn(final String username, final String password) {
+		super.navigateHome();
+		super.clickAndGo(By.linkText("Sign in"));
+		super.fill(By.id("username"), username);
+		super.fill(By.id("password"), password);
+		super.clickAndGo(By.id("remember$proxy"));
+		//super.submit(By.className("btn-primary"));
+	}
+
+	protected void signOut() {
+		super.navigateHome();
+		super.clickAndGo(By.linkText("Sign out"));
+	}
 	
 }
